768cac091f38a0269bfdd8fdac1dfafd87429562
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-10-04 15:39:56 +0200

    wsgi: allow redirect to any origin
    We allow any origin here as we are attempting to redirect
    to a different router's URL which is cross-site

--- turris_auth/server/wsgi.py
+++ turris_auth/server/wsgi.py
@@ -20,9 +20,16 @@ logger = logging.getLogger(__package__)
 
 STATUS_OK = f"{http.HTTPStatus.OK.value} {http.HTTPStatus.OK.phrase}"
 STATUS_FOUND = f"{http.HTTPStatus.FOUND.value} {http.HTTPStatus.FOUND.phrase}"
-STATUS_SEE_OTHER = f"{http.HTTPStatus.SEE_OTHER.value} {http.HTTPStatus.SEE_OTHER.phrase}"
-STATUS_UNAUTHORIZED = f"{http.HTTPStatus.UNAUTHORIZED.value} {http.HTTPStatus.UNAUTHORIZED.phrase}"
-STATUS_NOT_FOUND = f"{http.HTTPStatus.NOT_FOUND.value} {http.HTTPStatus.NOT_FOUND.phrase}"
+STATUS_SEE_OTHER = (
+    f"{http.HTTPStatus.SEE_OTHER.value} {http.HTTPStatus.SEE_OTHER.phrase}"
+)
+STATUS_UNAUTHORIZED = (
+    f"{http.HTTPStatus.UNAUTHORIZED.value} {http.HTTPStatus.UNAUTHORIZED.phrase}"
+)
+STATUS_NOT_FOUND = (
+    f"{http.HTTPStatus.NOT_FOUND.value} {http.HTTPStatus.NOT_FOUND.phrase}"
+)
+
 STATUS_SERVER_ERROR = f"{http.HTTPStatus.INTERNAL_SERVER_ERROR.value} {http.HTTPStatus.INTERNAL_SERVER_ERROR.phrase}"
 
 
@@ -33,7 +40,11 @@ class Server:
         self.report_invalid_password = report_invalid_password
         self.luci_login = luci_login
         self.wsgi = flup.server.fcgi.WSGIServer(
-            self._main, roles=(flup.server.fcgi_base.FCGI_RESPONDER, flup.server.fcgi_base.FCGI_AUTHORIZER)
+            self._main,
+            roles=(
+                flup.server.fcgi_base.FCGI_RESPONDER,
+                flup.server.fcgi_base.FCGI_AUTHORIZER,
+            ),
         )
         self.pages = pages.Pages()
 
@@ -51,10 +62,16 @@ class Server:
                     "POST/login": self._login_post,
                     "GET/logout": self._logout,
                     "GET/login.json": self._status,
-                }[f"{environ['REQUEST_METHOD']}{environ['SCRIPT_NAME']}"](environ, start_response)
+                }[f"{environ['REQUEST_METHOD']}{environ['SCRIPT_NAME']}"](
+                    environ, start_response
+                )
             except KeyError:
                 # The access to anything that is not implemented here is an error in configuration
-                logger.error("(%s) Accessing unimplemented URL: %s", environ["REMOTE_ADDR"], environ["SCRIPT_NAME"])
+                logger.error(
+                    "(%s) Accessing unimplemented URL: %s",
+                    environ["REMOTE_ADDR"],
+                    environ["SCRIPT_NAME"],
+                )
                 start_response(STATUS_NOT_FOUND, [("Content-type", "text/plain")])
                 return ["404 Not found"]
             except Exception:
@@ -67,17 +84,25 @@ class Server:
             start_response(STATUS_OK, [])
         else:
             if (environ.get("HTTP_X_REQUESTED_WITH") or "") == "":
-                start_response(STATUS_SEE_OTHER, [("Location", f"/login?{environ['REQUEST_URI']}")])
+                start_response(
+                    STATUS_SEE_OTHER,
+                    [
+                        ("Location", f"/login?{environ['REQUEST_URI']}"),
+                        # We allow here any origin as we are attempting redirect to different router's URL which is cross-site
+                        ("Access-Control-Allow-Origin", "*"),
+                    ],
+                )
             else:  # X-Requested-With header is not used by browser but should be specified by AJAX requests
                 start_response(STATUS_UNAUTHORIZED, [])
         return []
 
     def _login_new_session(self, environ, start_response):
         httpcookie = cookie.generate(
-            secure=environ["REQUEST_SCHEME"] == "https",
-            luci_login=self.luci_login
+            secure=environ["REQUEST_SCHEME"] == "https", luci_login=self.luci_login
         )
-        cookies = [("Set-Cookie", e.output(header="").strip()) for e in httpcookie.values()]
+        cookies = [
+            ("Set-Cookie", e.output(header="").strip()) for e in httpcookie.values()
+        ]
         start_response(
             STATUS_FOUND,
             [("Location", environ.get("QUERY_STRING") or "/")] + cookies,
@@ -86,13 +111,22 @@ class Server:
 
     def _login(self, environ, start_response):
         if cookie.verify(environ.get("HTTP_COOKIE"), self.luci_login):
-            start_response(STATUS_FOUND, [("Location", environ.get("QUERY_STRING") or "/")])
+            start_response(
+                STATUS_FOUND, [("Location", environ.get("QUERY_STRING") or "/")]
+            )
             return []
         if not password.is_set():
             return self._login_new_session(environ, start_response)
         # We allow here any origin as we are attempting redirect to https which is cross-site
-        start_response(STATUS_OK, [("Content-type", "text/html"), ("Access-Control-Allow-Origin", "*")])
-        return [self.pages.login(foris_language(), insecure=environ["REQUEST_SCHEME"] == "http")]
+        start_response(
+            STATUS_OK,
+            [("Content-type", "text/html"), ("Access-Control-Allow-Origin", "*")],
+        )
+        return [
+            self.pages.login(
+                foris_language(), insecure=environ["REQUEST_SCHEME"] == "http"
+            )
+        ]
 
     def _login_post(self, environ, start_response):
         form = cgi.FieldStorage(fp=environ["wsgi.input"], environ=environ)
@@ -105,16 +139,22 @@ class Server:
             )
             start_response(STATUS_UNAUTHORIZED, [("Content-type", "text/html")])
             return [
-                self.pages.login(foris_language(), wrongpass=True, insecure=environ["REQUEST_SCHEME"] == "http")
+                self.pages.login(
+                    foris_language(),
+                    wrongpass=True,
+                    insecure=environ["REQUEST_SCHEME"] == "http",
+                )
             ]
 
         return self._login_new_session(environ, start_response)
 
     def _logout(self, environ, start_response):
         httpcookie = cookie.remove(environ.get("HTTP_COOKIE"), self.luci_login)
-        cookies = [("Set-Cookie", e.output(header="").strip()) for e in httpcookie.values()]
+        cookies = [
+            ("Set-Cookie", e.output(header="").strip()) for e in httpcookie.values()
+        ]
         start_response(
-            STATUS_FOUND, 
+            STATUS_FOUND,
             [("Location", "/")] + cookies,
         )
         return []
@@ -122,6 +162,15 @@ class Server:
     @staticmethod
     def _status(environ, start_response):
         last_cookie_use = cookie.last_use(environ.get("HTTP_COOKIE"))
-        res = json.dumps({"accesstime": int(last_cookie_use.timestamp()) if last_cookie_use else None})
-        start_response(STATUS_OK, [("Content-type", "test/json"), ("Access-Control-Allow-Origin", "*")])
+        res = json.dumps(
+            {
+                "accesstime": int(last_cookie_use.timestamp())
+                if last_cookie_use
+                else None
+            }
+        )
+        start_response(
+            STATUS_OK,
+            [("Content-type", "test/json"), ("Access-Control-Allow-Origin", "*")],
+        )
         return [res]
